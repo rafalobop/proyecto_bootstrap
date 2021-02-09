05ef9508b83750786e91fbf6c49ab73ff1216e43
Rlobop <r.loboplaza@hotmail.com>
2021-02-09 13:00:30 -0300

    creadas las funciones javascript para los cards

--- js/index.js
+++ js/index.js
@@ -1,5 +1,32 @@
+
+//variables
+
 const irArriba = document.querySelector('.go__up')
 const banner = document.querySelector('#banner')
+const samsungBtn = document.querySelector('#samsung_btn')
+const motorolaBtn = document.querySelector('#motorola_btn')
+const iphoneBtn = document.querySelector('#apple_btn')
+const samsung = document.getElementById('samsung')
+const apple = document.getElementById('apple')
+const motorola = document.getElementById('motorola')
+const brands = document.querySelectorAll('.cards')
+
+
+//eventos
+
+irArriba.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+})
+
+samsungBtn.addEventListener('click', mostrarSamsung)
+motorolaBtn.addEventListener('click', mostrarMotorola)
+iphoneBtn.addEventListener('click', mostrarApple)
+
+
+//funciones
 
 window.onscroll = function () {
     if (document.documentElement.scrollTop > 100) {
@@ -8,9 +35,28 @@ window.onscroll = function () {
         irArriba.classList.remove('show')
     }
 }
-irArriba.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    })
-})
+
+function mostrarSamsung() {
+    if (samsung.style.display = 'none') {
+        samsung.style.display = 'block'
+        motorola.style.display = 'none'
+        apple.style.display = 'none'
+    }
+}
+
+function mostrarMotorola() {
+    if (motorola.style.display = 'none') {
+        motorola.style.display = 'block'
+        samsung.style.display = 'none'
+        apple.style.display = 'none'
+    }
+}
+function mostrarApple() {
+    if (apple.style.display = 'none') {
+        apple.style.display = 'block'
+        samsung.style.display = 'none'
+        motorola.style.display = 'none'
+    }
+}
+
+
